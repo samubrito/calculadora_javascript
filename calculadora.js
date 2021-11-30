d5afbf7fb256d4a313cd95b144ea6e8193b04b45
Samuel <samuelbrito599@gmail.com>
2021-11-30 11:44:08 -0300

    Calculadora
    Calculadora feita com HTML, CSS e
    JavaScript

---
+++ calculadora.js
@@ -0,0 +1,18 @@
+function inserir(num){
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num;
+}
+
+function limpar(){
+    document.getElementById('resultado').innerHTML = '';
+}
+
+function apagar(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0,resultado.length -1);
+}
+
+function calcular(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = eval(resultado);
+}
